ba526cd6e987aedf45ea4212818e283563d45d97
Adam Wathan <adam.wathan@gmail.com>
2017-05-02 16:02:45 -0400

    102 - QA Testing the Login Flow

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return "Laravel";
-});
-
-
 Route::get('/concerts/{id}', 'ConcertsController@show');
 Route::post('/concerts/{id}/orders', 'ConcertOrdersController@store');
 
