3922c6ba06e0db5a4cc4b9e1d2ddee922a0c8da5
Adam Wathan <adam.wathan@gmail.com>
2017-11-14 15:46:22 -0500

    (change minimum poster width from 400px to 600px)

--- tests/Feature/Backstage/AddConcertTest.php
+++ tests/Feature/Backstage/AddConcertTest.php
@@ -365,11 +365,11 @@ class AddConcertTest extends TestCase
     }
 
     /** @test */
-    public function poster_image_must_be_at_least_400px_wide()
+    public function poster_image_must_be_at_least_600px_wide()
     {
         Storage::fake('public');
         $user = factory(User::class)->create();
-        $file = File::image('poster.png', 399, 516);
+        $file = File::image('poster.png', 599, 775);
 
         $response = $this->actingAs($user)->from('/backstage/concerts/new')->post('/backstage/concerts', $this->validParams([
             'poster_image' => $file,
