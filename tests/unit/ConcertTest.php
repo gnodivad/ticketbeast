ea82a0cfbcef1e310bec10c110230bfc44cf45c1
David Ong <dodwei.dev@gmail.com>
2018-01-07 10:21:07 +0800

    1.5 - Refactoring for Speed

--- tests/unit/ConcertTest.php
+++ tests/unit/ConcertTest.php
@@ -7,17 +7,30 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ConcertTest extends TestCase
 {
-    use DatabaseMigrations;
     /** @test */
     public function can_get_formatted_date()
     {
-        // Create a concert with a known date
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([
             'date' => Carbon::parse('2016-12-01 8:00pm'),
         ]);
-        // Retrieve the formatted date
-        $date = $concert->formatted_date;
-        // Verify the date is formatted as expected
-        $this->assertEquals('December 1, 2016', $date);
+        $this->assertEquals('December 1, 2016', $concert->formatted_date);
+    }
+
+    /** @test */
+    public function can_get_formatted_start_time()
+    {
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2016-12-01 17:00:00'),
+        ]);
+        $this->assertEquals('5:00pm', $concert->formatted_start_time);
+    }
+    
+    /** @test */
+    public function can_get_ticket_price_in_dollars()
+    {
+        $concert = factory(Concert::class)->make([
+            'ticket_price' => 6750,
+        ]);
+        $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
     }
 }
