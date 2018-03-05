6746514e81bc823106692127dac1cc5f7160d235
DavidODW <dodwei.dev@gmail.com>
2018-03-05 09:44:23 +0800

    3.5 - Cancelling Failed Orders

---
+++ tests/unit/OrderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Concert;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Order;
+
+class OrderTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function tickets_are_released_when_an_order_is_cancelled()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(10);
+        $order = $concert->orderTickets('jane@example.com', 5);
+        $this->assertEquals(5, $concert->ticketsRemaining());
+
+        $order->cancel();
+
+        $this->assertEquals(10, $concert->ticketsRemaining());
+        $this->assertNull(Order::find($order->id));
+    }
+}
