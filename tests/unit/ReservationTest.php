b784c4935afc73b9979a7c50fde9f46a3d557efa
DavidODW <dodwei.dev@gmail.com>
2018-03-06 14:04:21 +0800

    6.2 - You Might Not Need a Mocking Library

--- tests/unit/ReservationTest.php
+++ tests/unit/ReservationTest.php
@@ -1,18 +1,20 @@
 <?php
 
-use App\Concert;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Reservation;
 
-class RevervationTest extends TestCase
+class ReservationTest extends TestCase
 {
     use DatabaseMigrations;
 
     /** @test */
     public function calculating_the_total_cost()
     {
-        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(3);
-        $tickets = $concert->findTickets(3);
+        $tickets = collect([
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+            (object) ['price' => 1200]
+        ]);
 
         $reservation = new Reservation($tickets);
 
