d67f34af9f10b1e120ea7664a5f614917a6767c7
DavidODW <dodwei.dev@gmail.com>
2018-03-06 16:16:04 +0800

    6.7 - Replicating the Failure at the Unit Level

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -30,7 +30,7 @@ class ConcertOrdersController extends Controller
         ]);
 
         try {
-            $tickets = $concert->findTickets(request('ticket_quantity'));
+            $tickets = $concert->reserveTickets(request('ticket_quantity'));
             $reservation = new Reservation($tickets);
 
             $this->paymentGateway->charge($reservation->totalCost(), request('payment_token'));
