2868cb0a8e3629b229a5d208fc969e57c6734b93
DavidODW <dodwei.dev@gmail.com>
2018-03-06 09:03:56 +0800

    4.2 - Returning Order Details

--- tests/unit/OrderTest.php
+++ tests/unit/OrderTest.php
@@ -9,6 +9,21 @@ class OrderTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    public function converting_to_an_array()
+    {
+        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(5);
+        $order = $concert->orderTickets('jane@example.com', 5);
+
+        $result = $order->toArray();
+
+        $this->assertEquals([
+            'email' => 'jane@example.com',
+            'ticket_quantity' => 5,
+            'amount' => 6000
+        ], $result);
+    }
+
+    /** @test */
     public function tickets_are_released_when_an_order_is_cancelled()
     {
         $concert = factory(Concert::class)->create()->addTickets(10);
