75f5b494c714985b8297edec347355151d8b9bfa
DavidODW <dodwei.dev@gmail.com>
2018-03-09 10:46:08 +0800

    48 - Cleaning up a Loose Variable

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -8,7 +8,6 @@ use App\Concert;
 use App\Billing\PaymentFailedException;
 use App\Exceptions\NotEnoughTicketsException;
 use App\Order;
-use App\Reservation;
 
 class ConcertOrdersController extends Controller
 {
@@ -30,12 +29,11 @@ class ConcertOrdersController extends Controller
         ]);
 
         try {
-            $tickets = $concert->reserveTickets(request('ticket_quantity'));
-            $reservation = new Reservation($tickets);
+            $reservation = $concert->reserveTickets(request('ticket_quantity'));
 
             $this->paymentGateway->charge($reservation->totalCost(), request('payment_token'));
 
-            $order = Order::forTickets($tickets, request('email'), $reservation->totalCost());
+            $order = Order::forTickets($reservation->tickets(), request('email'), $reservation->totalCost());
 
             return response()->json($order, 201);
         } catch (PaymentFailedException $e) {
--- tests/unit/ReservationTest.php
+++ tests/unit/ReservationTest.php
@@ -22,6 +22,20 @@ class ReservationTest extends TestCase
     }
 
     /** @test */
+    public function retrieving_the_reservations_tickets()
+    {
+        $tickets = collect([
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+            (object) ['price' => 1200]
+        ]);
+
+        $reservation = new Reservation($tickets);
+
+        $this->assertEquals($tickets, $reservation->tickets());
+    }
+
+    /** @test */
     public function reserved_tickets_are_released_when_a_reservation_is_cancelled()
     {
         $tickets = collect([
