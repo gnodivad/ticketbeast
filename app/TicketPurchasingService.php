9e01738572112973a4365839fcb9490199077ed5
DavidODW <dodwei.dev@gmail.com>
2018-03-09 16:17:21 +0800

    Add TicketPurchasingService

---
+++ app/TicketPurchasingService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use App\Billing\PaymentGateway;
+
+class TicketPurchasingService
+{
+    private $paymentGateway;
+
+    public function __construct(PaymentGateway $paymentGateway)
+    {
+        $this->paymentGateway = $paymentGateway;
+    }
+
+    public function purchaseTickets($concert, $ticketQuantity, $email, $paymentToken)
+    {
+        $reservation = $concert->reserveTickets($ticketQuantity, $email);
+
+        $this->paymentGateway->charge($reservation->totalCost(), $paymentToken);
+
+        return Order::forTickets($reservation->tickets(), $reservation->email(), $reservation->totalCost());
+    }
+}
