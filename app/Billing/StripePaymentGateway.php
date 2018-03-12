c587d0cda9e54df4f596900188714d4ba0d9bd30
DavidODW <dodwei.dev@gmail.com>
2018-03-12 13:30:21 +0800

    55 - Making a Successful Charge

---
+++ app/Billing/StripePaymentGateway.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Billing;
+
+use Stripe\Charge;
+
+class StripePaymentGateway implements PaymentGateway
+{
+    private $apiKey;
+
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function charge($amount, $token)
+    {
+        Charge::create(
+            [
+                'amount' => $amount,
+                'source' => $token,
+                'currency' => 'USD'
+            ],
+            ['api_key' => $this->apiKey]
+        );
+    }
+}
--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -1,14 +1,13 @@
 <?php
 
+use App\Billing\StripePaymentGateway;
 
 class StripePaymentGatewayTest extends TestCase
 {
     /** @test */
     public function charges_with_a_valid_payment_token_are_successful()
     {
-        $paymentGateway = new StripePaymentGateway;
-
-        \Stripe\Stripe::setApiKey(config('services.stripe.secret'));
+        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
 
         $token = \Stripe\Token::create([
             'card' => [
@@ -21,10 +20,12 @@ class StripePaymentGatewayTest extends TestCase
 
         $paymentGateway->charge(2500, $token);
 
-        $lastCharge = \Stripe\Charge::all([
-            'limit' => 1,
-            ['api_key' => config('services.stripe.secret')]
-        ])['data'][0];
+        $lastCharge = \Stripe\Charge::all(
+            [
+            'limit' => 1
+        ],
+        ['api_key' => config('services.stripe.secret')]
+        )['data'][0];
 
         $this->assertEquals(2500, $lastCharge->amount);
     }
