5ac376ae2c3d72de384472ab1a98ae4ad02d9aa4
DavidODW <dodwei.dev@gmail.com>
2018-03-12 09:58:20 +0800

    53 - Generating a Valid Payment Token

---
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class StripePaymentGatewayTest extends TestCase
+{
+    /** @test */
+    public function charges_with_a_valid_payment_token_are_successful()
+    {
+        $paymentGateway = new StripePaymentGateway;
+
+        \Stripe\Stripe::setApiKey(config('services.stripe.secret'));
+
+        $token = \Stripe\Token::create([
+            'card' => [
+                'number' => '4242424242424242',
+                'exp_month' => 1,
+                'exp_year' => date('Y') + 1,
+                'cvc' => '123'
+            ]
+            ], ['api_key' => config('services.stripe.secret')])->id;
+
+        $paymentGateway->charge(2500, $token);
+    }
+}
