ed5d34a54060011fca62f796b7dafa336d031ac3
DavidODW <dodwei.dev@gmail.com>
2018-03-13 09:35:44 +0800

    56 - Dealing with Lingering State

--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -4,29 +4,52 @@ use App\Billing\StripePaymentGateway;
 
 class StripePaymentGatewayTest extends TestCase
 {
-    /** @test */
-    public function charges_with_a_valid_payment_token_are_successful()
+    private function lastCharge()
     {
-        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
+        return array_first(\Stripe\Charge::all(
+            [
+                'limit' => 1
+            ],
+            ['api_key' => config('services.stripe.secret')]
+        )['data']);
+    }
 
-        $token = \Stripe\Token::create([
+    private function newCharges()
+    {
+        return \Stripe\Charge::all(
+            [
+                'ending_before' => $this->lastCharge ? $this->lastCharge->id : null,
+            ],
+            ['api_key' => config('services.stripe.secret')]
+        )['data'];
+    }
+
+    private function validToken()
+    {
+        return \Stripe\Token::create([
             'card' => [
                 'number' => '4242424242424242',
                 'exp_month' => 1,
                 'exp_year' => date('Y') + 1,
                 'cvc' => '123'
             ]
-            ], ['api_key' => config('services.stripe.secret')])->id;
+        ], ['api_key' => config('services.stripe.secret')])->id;
+    }
 
-        $paymentGateway->charge(2500, $token);
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->lastCharge = $this->lastCharge();
+    }
 
-        $lastCharge = \Stripe\Charge::all(
-            [
-            'limit' => 1
-        ],
-        ['api_key' => config('services.stripe.secret')]
-        )['data'][0];
+    /** @test */
+    public function charges_with_a_valid_payment_token_are_successful()
+    {
+        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
 
-        $this->assertEquals(2500, $lastCharge->amount);
+        $paymentGateway->charge(2500, $this->validToken());
+
+        $this->assertCount(1, $this->newCharges());
+        $this->assertEquals(2500, $this->lastCharge()->amount);
     }
 }
