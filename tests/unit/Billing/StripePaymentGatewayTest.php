2aa941b3c1c90ec674121f7c50f349e2e5c50ed1
DavidODW <dodwei.dev@gmail.com>
2018-03-14 10:30:08 +0800

    61 - When Interfaces Aren't Enough

--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Billing\StripePaymentGateway;
-use App\Billing\PaymentFailedException;
 
 /**
  * @group integration
@@ -60,14 +59,8 @@ class StripePaymentGatewayTest extends TestCase
     /** @test */
     public function charges_with_an_invalid_payment_token_fail()
     {
-        try {
-            $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
-            $paymentGateway->charge(2500, 'invalid-payment-token');
-        } catch (PaymentFailedException $e) {
-            $this->assertCount(0, $this->newCharges());
-            return;
-        }
-
-        $this->fail('Charging with an invalid payment token did not throw a PaymentFailedException');
+        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
+        $result = $paymentGateway->charge(2500, 'invalid-payment-token');
+        $this->assertFalse($result);
     }
 }
