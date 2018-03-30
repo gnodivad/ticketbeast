f597afa8ec516200b3e3506bc8b610fcb952ef09
David Ong <dodwei.dev@gmail.com>
2018-03-30 15:13:54 +0800

    79 - Confirmation Number Characteristics

---
+++ tests/unit/RandomOrderConfirmationNumberGeneratorTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class RandomOrderConfirmationNumberGeneratorTest extends TestCase
+{
+}
