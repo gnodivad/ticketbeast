71138f01c549e03ae85722aa046864466bd198b8
David Ong <dodwei.dev@gmail.com>
2018-03-30 15:24:40 +0800

    80 - Testing the Confirmation Number Format

--- tests/unit/RandomOrderConfirmationNumberGeneratorTest.php
+++ tests/unit/RandomOrderConfirmationNumberGeneratorTest.php
@@ -3,7 +3,40 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\RandomOrderConfirmationNumberGenerator;
 
 class RandomOrderConfirmationNumberGeneratorTest extends TestCase
 {
+    /** @test */
+    public function must_be_24_characters_long()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertEquals(24, strlen($confirmationNumber));
+    }
+
+    /** @test */
+    public function can_only_contain_uppercase_letters_and_numbers()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertRegExp('/^[A-Z0-9]+$/', $confirmationNumber);
+    }
+
+    /** @test */
+    public function cannot_contain_ambiguous_characters()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertFalse(strpos($confirmationNumber, '1'));
+        $this->assertFalse(strpos($confirmationNumber, 'I'));
+        $this->assertFalse(strpos($confirmationNumber, '0'));
+        $this->assertFalse(strpos($confirmationNumber, 'O'));
+    }
 }
