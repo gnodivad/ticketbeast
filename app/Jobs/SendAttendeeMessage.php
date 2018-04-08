8ed25956d4b16598845423bfe1e07b9aa0997c6e
David Ong <dodwei.dev@gmail.com>
2018-04-08 11:36:58 +0800

    135 - Unit Testing the Job

--- app/Jobs/SendAttendeeMessage.php
+++ app/Jobs/SendAttendeeMessage.php
@@ -7,6 +7,8 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\AttendeeMessageEmail;
 
 class SendAttendeeMessage implements ShouldQueue
 {
@@ -31,6 +33,8 @@ class SendAttendeeMessage implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $this->attendeeMessage->recipients()->each(function ($recipient) {
+            Mail::to($recipient)->send(new AttendeeMessageEmail($this->attendeeMessage));
+        });
     }
 }
---
+++ tests/Unit/Jobs/SendAttendeeMessageTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Unit\Jobs;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use App\AttendeeMessage;
+use App\Jobs\SendAttendeeMessage;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\AttendeeMessageEmail;
+
+class SendAttendeeMessageTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function it_sends_the_message_to_all_concert_attendees()
+    {
+        Mail::fake();
+
+        $concert = \ConcertFactory::createPublished();
+        $otherConcert = \ConcertFactory::createPublished();
+
+        $message = AttendeeMessage::create([
+            'concert_id' => $concert->id,
+            'subject' => 'My subject',
+            'message' => 'My message'
+        ]);
+        $orderA = \OrderFactory::createForConcert($concert, ['email' => 'alex@example.com']);
+        $orderOrder = \OrderFactory::createForConcert($otherConcert, ['email' => 'jane@example.com']);
+        $orderB = \OrderFactory::createForConcert($concert, ['email' => 'sam@example.com']);
+        $orderC = \OrderFactory::createForConcert($concert, ['email' => 'taylor@example.com']);
+
+        SendAttendeeMessage::dispatch($message);
+
+        Mail::assertSent(AttendeeMessageEmail::class, function ($mail) use ($message) {
+            return $mail->hasTo('alex@example.com')
+                && $mail->attendeeMessage->is($message);
+        });
+        Mail::assertSent(AttendeeMessageEmail::class, function ($mail) use ($message) {
+            return $mail->hasTo('sam@example.com')
+                && $mail->attendeeMessage->is($message);
+        });
+        Mail::assertSent(AttendeeMessageEmail::class, function ($mail) use ($message) {
+            return $mail->hasTo('taylor@example.com')
+                && $mail->attendeeMessage->is($message);
+        });
+        Mail::assertNotSent(AttendeeMessageEmail::class, function ($mail) use ($message) {
+            return $mail->hasTo('jabe@example.com');
+        });
+    }
+}
