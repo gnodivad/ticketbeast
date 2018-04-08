abc6a94655e7d9ea9b14c82be2d839a11f02df7c
David Ong <dodwei.dev@gmail.com>
2018-04-08 10:56:54 +0800

    133 - Storing Messages for Attendees

--- tests/Feature/Backstage/MessageAttendeesTest.php
+++ tests/Feature/Backstage/MessageAttendeesTest.php
@@ -6,13 +6,14 @@ use App\User;
 use ConcertFactory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\AttendeeMessage;
 
 class MessageAttendeesTest extends TestCase
 {
     use DatabaseMigrations;
 
     /** @test */
-    function a_promoter_can_view_the_message_form_for_their_own_concert()
+    public function a_promoter_can_view_the_message_form_for_their_own_concert()
     {
         $this->disableExceptionHandling();
 
@@ -29,7 +30,7 @@ class MessageAttendeesTest extends TestCase
     }
 
     /** @test */
-    function a_promoter_cannot_view_the_message_form_for_another_concert()
+    public function a_promoter_cannot_view_the_message_form_for_another_concert()
     {
         $user = factory(User::class)->create();
         $concert = ConcertFactory::createPublished([
@@ -42,12 +43,35 @@ class MessageAttendeesTest extends TestCase
     }
 
     /** @test */
-    function a_guest_cannot_view_the_message_form_for_any_concert()
+    public function a_guest_cannot_view_the_message_form_for_any_concert()
     {
         $concert = ConcertFactory::createPublished();
 
         $response = $this->get("/backstage/concerts/{$concert->id}/messages/new");
 
         $response->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function a_promoter_can_send_a_new_message()
+    {
+        $user = factory(User::class)->create();
+
+        $concert = ConcertFactory::createPublished([
+            'user_id' => $user->id
+        ]);
+
+        $response = $this->actingAs($user)->post("/backstage/concerts/{$concert->id}/messages", [
+            'subject' => 'My subject',
+            'message' => 'My message'
+        ]);
+
+        $response->assertRedirect("/backstage/concerts/{$concert->id}/messages/new");
+        $response->assertSessionHas('flash');
+
+        $message = AttendeeMessage::first();
+        $this->assertEquals($concert->id, $message->concert_id);
+        $this->assertEquals('My subject', $message->subject);
+        $this->assertEquals('My message', $message->message);
     }
 }
