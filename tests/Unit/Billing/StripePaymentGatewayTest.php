37918cdd4b58d37020e8092eacf65251a14ca9e3
David Ong <dodwei.dev@gmail.com>
2018-04-14 08:56:09 +0800

    163 - Total Charges for a Specific Account

--- tests/Unit/Billing/StripePaymentGatewayTest.php
+++ tests/Unit/Billing/StripePaymentGatewayTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Billing;
 use Tests\TestCase;
 use App\Billing\StripePaymentGateway;
 
+/** @group integration */
 class StripePaymentGatewayTest extends TestCase
 {
     use PaymentGatewayContractTests;
